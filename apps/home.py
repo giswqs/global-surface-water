d70cf4d073124ee8ee148208856492835b93b1f7
Qiusheng Wu <giswqs@gmail.com>
2022-10-02 00:21:40 -0400

    Updated homepage

--- apps/home.py
+++ apps/home.py
@@ -7,11 +7,13 @@ def app():
 
     st.markdown(
         """
-    Select **Datasets** or **Split-panel Map** from the Main Menu to explore global surface water datasets interatively.
+    The Global Surface Water Information System (GSWIS) brings the emerging regional and global datasets of surface water under one platform. 
+    This global platform allows users to instantaneously visualize and compare different datasets, understand their variations, and therefore 
+    select datasets that are most suitable for a specific research, management, or policy decision.
 
     """
     )
-    st.image("https://i.imgur.com/KVRvlwM.png")
+    st.image("https://i.imgur.com/7eyMcZQ.gif")
 
     # m = leafmap.Map(locate_control=True)
     # m.add_basemap("ROADMAP")
